0aa663e669d7e963c1be46577d0224ecd9487974
ricRobo4 <artistr3@proton.me>
2022-10-27 15:31:53 -0700

    recieved peer review

--- Controllers/AskingQuestionsController.cs
+++ Controllers/AskingQuestionsController.cs
@@ -1,3 +1,8 @@
+// Richard Johnson
+// 10-25-22
+// This API will take the name and time the user typed into the URL, and save them as strings so they can be printed on the screen
+// Peer Review: Marcel R. Program ran smooth, took two strings as name and time and output a message saying the name and time woke up. It also had a nice message :)
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace JohnsonRAskingQuestionsEndpoint.Controllers;
